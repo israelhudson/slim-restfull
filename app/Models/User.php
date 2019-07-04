cbde26d1fff4ca867adf275d4da21ced72c2a3e0
Israel Hudson <israelhudson@gmail.com>
2019-07-04 15:13:24 -0300

    crud completo

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,5 +10,13 @@ class User extends Eloquent
 
 {
     protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password'];
 
+    public function setPasswordAttribute($value)
+    {
+        if(!empty($value)){
+            $this->attributes['password'] = password_hash($value, PASSWORD_DEFAULT);
+        }
+
+    }
 }
--- src/routes/users.php
+++ src/routes/users.php
@@ -15,3 +15,20 @@ $app->post('/api/v1/users', function (Request $request, Response $response, arra
     $user = User::create($data);
     return $response->withJson($user);
 });
+
+$app->get('/api/v1/users/{id}', function (Request $request, Response $response, array  $args){
+    $user = User::findOrFail($args['id']);
+    return $response->withJson($user);
+});
+
+$app->put('/api/v1/users/{id}', function (Request $request, Response $response, array  $args){
+    $user = User::findOrFail($args['id']);
+    $user->update($request->getParsedBody());
+    return $response->withJson($user);
+});
+
+$app->delete('/api/v1/users/{id}', function (Request $request, Response $response, array  $args){
+    $user = User::findOrFail($args['id']);
+    $user->delete();
+    return $response->withJson($user);
+});
